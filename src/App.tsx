711dba8bd99bed7ccfd47876b7f3b4abdf7fcdfd
JAlejandroQuevedo <alejandro_ya23@hotmail.com>
2024-11-26 11:47:17 -0700

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  console.log(`Hola desde ${process.env.MY_SECRET_KEY}`)
+  console.log(`Hola desde ${process.env.MY_SECRET_KEY} holis`)
   return (
     <>
       <div>
