80027e74716bbd75369e43e48d845ea4883cd2af
JAlejandroQuevedo <alejandro_ya23@hotmail.com>
2024-11-26 14:18:22 -0700

    Commit azure front

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import dotenv from 'dotenv';
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0);
-  dotenv.config()
-  const holi = process.env.DATABASE_CONNECTION_STRING;
+  const holi = import.meta.env.VITE_API_KEY;
   console.log(`Hola desde ${holi} holis`)
   return (
     <>
