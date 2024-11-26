ed291b600d9c7a30bda000467d9228c5c6750c2c
JAlejandroQuevedo <alejandro_ya23@hotmail.com>
2024-11-26 12:39:27 -0700

    Dot env

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import dotenv from 'dotenv';
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0);
+  dotenv.config()
   const holi = process.env.DATABASE_CONNECTION_STRING;
   console.log(`Hola desde ${holi} holis`)
   return (
